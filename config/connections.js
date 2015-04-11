6ad481d2645911731143e23edccb21554301ca80
vuongngo <vuongngo.pd@gmail.com>
2015-04-12 00:37:38 +1000

    Tested

--- config/connections.js
+++ config/connections.js
@@ -75,7 +75,7 @@ module.exports.connections = {
   //   },
   // },
 
-  'oseam': {
+  'oseamDb': {
     adapter: 'sails-mongo',
     url: process.env.MONGOLAB_URI
   },
--- config/models.js
+++ config/models.js
@@ -6,7 +6,7 @@ module.exports.models = {
   * connections (see `config/connections.js`)                                *
   *                                                                          *
   ***************************************************************************/
-  connection: 'oseam',
+  connection: 'oseamDb',
 
   /***************************************************************************
   *                                                                          *
