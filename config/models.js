fd7c06b6a1245f57122a2815ed136a156f092f51
vuongngo <vuongngo.pd@gmail.com>
2015-04-11 23:56:29 +1000

    Tested

--- config/models.js
+++ config/models.js
@@ -29,53 +29,53 @@ module.exports.models = {
   ***************************************************************************/
   migrate: 'safe',
 
-  seed: function (callback) {
-    var self = this;
-    var modelName = self.adapter.identity.charAt(0).toUpperCase() + self.adapter.identity.slice(1);
-    if (!self.seedData) {
-      sails.log.debug('No data avaliable to seed ' + modelName);
-      callback();
-      return;
-    }
-    self.count().exec(function (err, count) {
-      if (!err && count === 0) {
-        sails.log.debug('Seeding ' + modelName + '...');
-        if (self.seedData instanceof Array) {
-          self.seedArray(callback);
-        }else{
-          self.seedObject(callback);
-        }
-      } else {
-        sails.log.debug(modelName + ' had models, so no seed needed');
-        callback();
-      }
-    });
-  },
-  seedArray: function (callback) {
-    var self = this;
-    var modelName = self.adapter.identity.charAt(0).toUpperCase() + self.adapter.identity.slice(1);
-    self.createEach(self.seedData).exec(function (err, results) {
-      if (err) {
-        sails.log.debug(err);
-        callback();
-      } else {
-        sails.log.debug(modelName + ' seed planted');
-        callback();
-      }
-    });
-  },
-  seedObject: function (callback) {
-    var self = this;
-    var modelName = self.adapter.identity.charAt(0).toUpperCase() + self.adapter.identity.slice(1);
-    self.create(self.seedData).exec(function (err, results) {
-      if (err) {
-        sails.log.debug(err);
-        callback();
-      } else {
-        sails.log.debug(modelName + ' seed planted');
-        callback();
-      }
-    });
+  // seed: function (callback) {
+  //   var self = this;
+  //   var modelName = self.adapter.identity.charAt(0).toUpperCase() + self.adapter.identity.slice(1);
+  //   if (!self.seedData) {
+  //     sails.log.debug('No data avaliable to seed ' + modelName);
+  //     callback();
+  //     return;
+  //   }
+  //   self.count().exec(function (err, count) {
+  //     if (!err && count === 0) {
+  //       sails.log.debug('Seeding ' + modelName + '...');
+  //       if (self.seedData instanceof Array) {
+  //         self.seedArray(callback);
+  //       }else{
+  //         self.seedObject(callback);
+  //       }
+  //     } else {
+  //       sails.log.debug(modelName + ' had models, so no seed needed');
+  //       callback();
+  //     }
+  //   });
+  // },
+  // seedArray: function (callback) {
+  //   var self = this;
+  //   var modelName = self.adapter.identity.charAt(0).toUpperCase() + self.adapter.identity.slice(1);
+  //   self.createEach(self.seedData).exec(function (err, results) {
+  //     if (err) {
+  //       sails.log.debug(err);
+  //       callback();
+  //     } else {
+  //       sails.log.debug(modelName + ' seed planted');
+  //       callback();
+  //     }
+  //   });
+  // },
+  // seedObject: function (callback) {
+  //   var self = this;
+  //   var modelName = self.adapter.identity.charAt(0).toUpperCase() + self.adapter.identity.slice(1);
+  //   self.create(self.seedData).exec(function (err, results) {
+  //     if (err) {
+  //       sails.log.debug(err);
+  //       callback();
+  //     } else {
+  //       sails.log.debug(modelName + ' seed planted');
+  //       callback();
+  //     }
+  //   });
  
-  }
+  // }
 };
