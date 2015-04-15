6d3c642b6faa63a1929d411c4dfadeaf420ca2a7
vuongngo <vuongngo.pd@gmail.com>
2015-04-15 10:31:00 +1000

    Transaction

--- api/policies/isUser.js
+++ api/policies/isUser.js
@@ -2,6 +2,6 @@ var expressJwt = require('express-jwt');
 var secret = sails.config.session.secret;
 
 module.exports = expressJwt({secret: secret}, function (req, res) {
-	if (!req.user) return res.send(401)
+	if (!req.user) return res.send(401);
 	res.send(200);
 });
