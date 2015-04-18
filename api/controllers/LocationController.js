2bc0cf9fb1574ad640b6274228488f3387dccffc
vuongngo <vuongngo.pd@gmail.com>
2015-04-18 17:10:34 +1000

    Ip lookup

--- api/controllers/LocationController.js
+++ api/controllers/LocationController.js
@@ -6,18 +6,23 @@ module.exports = {
 
   ip_lookup: function (req, res) {
 
-    var geoip = require('geoip-lite');
-
+    // var geoip = require('geoip-lite');
+    var request = require('request');
     var ip = req.headers['x-forwarded-for'] || 
      req.connection.remoteAddress || 
      req.socket.remoteAddress ||
      req.connection.socket.remoteAddress;
 
-    var geo = geoip.lookup(ip);
+    // var geo = geoip.lookup(ip);
 
-    if (!geo) return res.notFound();
+    // if (!geo) return res.notFound();
 
-    res.ok(geo);
+    // res.ok(geo);
+      url = 'http://freegeoip.net/json/' + ip
+      request.get(url, function(error, response, body) {
+        if (error) return res.notFound();
+        res.ok(body);
+      }
   },
     
 }
