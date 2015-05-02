cc3121dedba86510e69122dd145f0352f24f669a
vuongngo <vuongngo.pd@gmail.com>
2015-05-02 17:03:52 +1000

    Seed data

--- config/connections.js
+++ config/connections.js
@@ -62,24 +62,24 @@ module.exports.connections = {
     url: process.env.MONGOLAB_URI
   },
 
-  // hackathon: {
-  //   adapter: 'sails-mongo',
-  //   host: 'localhost',
-  //   port: 27017,
-  //   user: '',
-  //   password: '',
-  //   database: 'hackathondb',
+  hackathon: {
+    adapter: 'sails-mongo',
+    host: 'localhost',
+    port: 27017,
+    user: '',
+    password: '',
+    database: 'hackathondb',
 
-  //   // Server Options
-  //   ssl: false,
-  //   poolSize: 10,
-  //   socketOptions: {
-  //     noDelay: true,
-  //     keepAlive: 1,
-  //     connectTimeoutMS: 30000,
-  //     socketTimeoutMS: 30000
-  //   },
-  // },
+    // Server Options
+    ssl: false,
+    poolSize: 10,
+    socketOptions: {
+      noDelay: true,
+      keepAlive: 1,
+      connectTimeoutMS: 30000,
+      socketTimeoutMS: 30000
+    },
+  },
   /***************************************************************************
   *                                                                          *
   * PostgreSQL is another officially supported relational database.          *
