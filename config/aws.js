1a23e443e3f94aac8f4ec5b51919cf1d6b36802c
vuongngo <vuongngo.pd@gmail.com>
2015-06-16 16:01:41 +1000

    Publish

--- config/aws.js
+++ config/aws.js
@@ -1,6 +1,6 @@
 // config/aws.js
 module.exports.aws = {
-  key: 'AKIAJPKCBYUVBRBLAEGQ',
-  secret: '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',
-  bucket: 'cheerseeng'
+  key: 'xxx',
+  secret: 'xxx',
+  bucket: 'xxx'
 };
--- config/stripe.js
+++ config/stripe.js
@@ -1,9 +1,9 @@
 // config/stripe.js
 module.exports.stripe = {
-  testClientId: 'ca_60rzt4IsHzlWikredHVTxZHAlNDhDvaC',
-  testSecretKey: 'sk_test_oPJOrwhkdFhK8pWfMRLXBaqv',
-  testPublishableKey: 'pk_test_91ap7sNejQqUBOQJD3jD00oY',
-  liveClientId: 'ca_60rzhJJNZg7VkepfiwZhUCVDxkriYrNj',
-  liveSecretKey: 'sk_live_MEAKmSUE8BQMKiyZSF0jeXYG',
-  livePublishableKey: 'pk_live_5lyWRXXEZ2XgJhdVXTbWbWQz'
+  testClientId: 'xxx',
+  testSecretKey: 'xxx',
+  testPublishableKey: 'xxx',
+  liveClientId: 'xxx',
+  liveSecretKey: 'xxx',
+  livePublishableKey: 'xxx'
 };
